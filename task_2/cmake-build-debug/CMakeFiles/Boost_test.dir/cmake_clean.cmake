25a9a6833fd01526e406127c5e164cfd5230da54
Arina Lapteva <arina.lapteva2016@yandex.ru>
2022-12-16 01:04:20 +0300

    assembly cmake file

---
+++ task_2/cmake-build-debug/CMakeFiles/Boost_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Boost_test.exe"
+  "Boost_test.exe.manifest"
+  "Boost_test.pdb"
+  "CMakeFiles/Boost_test.dir/client.cpp.obj"
+  "CMakeFiles/Boost_test.dir/main.cpp.obj"
+  "libBoost_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Boost_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
